7d1c95dc3a3554f41585b978e09ffc8fc2c9546d
gavin-dale <63865924+gavin-dale@users.noreply.github.com>
2021-02-19 12:14:10 -0700

    Add files via upload

---
+++ SumCalculator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("Vance");
+        System.out.println(person.getFullName());
+    }
+}
